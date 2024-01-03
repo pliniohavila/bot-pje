517fdccf22df861d3e83ff60427eb829319df944
pliniohrcode <pliniohrcode@gmail.com>
2024-01-03 12:21:32 -0300

    Correções simples para se adequar a atual versão do PJE TJMG

--- index.js
+++ index.js
@@ -20,19 +20,74 @@ const urlLogin = 'https://pje.tjmg.jus.br/pje/login.seam';
 
 const chromeOptions = {
     headless: true,
-    defaultViewport: null
+    defaultViewport: null,
+    args: [
+        '--no-first-run', //  Impede que o navegador exiba a página de boas-vindas na primeira execução.
+        '--no-sandbox', // "Disables the sandbox for all process types that are normally sandboxed.
+        '--disable-setuid-sandbox', // "Disables the sandbox for all process types that are normally sandboxed.
+        '--disable-sync', // "Disables syncing browser data to a Google Account"
+        '--disable-dev-shm-usage', // The /dev/shm partition is too small in certain VM environments, causing Chrome to fail or crash (see http://crbug.com/715363). 
+        '--disable-gpu',
+        "--disable-accelerated-2d-canvas",
+        '--ignore-certificate-errors',
+        '--enable-logging,', 
+        '--v=1'
+    ],
 };
 
 (async function main() {
     const browser = await puppeteer.launch(chromeOptions);
     const page = await browser.newPage();
+    await page.setUserAgent(
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0');
+    await page.setRequestInterception(true);
+
+    // Para não fazer requisições de imagens e fonts. 
+    // Assim, o carregamento da página fica mais rápido
+    page.on('request', (request) => {
+        if (['image', 'font'].indexOf(request.resourceType()) !== -1) {
+            request.respond({status: 200, body: 'aborted'})
+        } else {
+            request.continue();
+        }
+    });
+    
+    page.on('requestfailed', request => {
+        console.log(request.url() + ' ' + request.failure().errorText);
+    });
+
     // LOGIN
     await page.goto(urlLogin);
-    const cpf = await page.waitForSelector('input[placeholder="CPF / CNPJ"]');
-    const pass = await page.waitForSelector('input[placeholder="Senha"]');
+    // const cpf = await page.waitForSelector('input[placeholder="CPF / CNPJ"]');
+    // const pass = await page.waitForSelector('input[placeholder="Senha"]');
+    // Houve versão do PJE em que o login estava contido em um <iframe>
+    const elementHandle = await page.$('iframe[id="ssoFrame"]');
+    const frameLogin =  await elementHandle.contentFrame();
+    const cpf = await frameLogin.waitForSelector('input[id="username"]', {visible: true});
+    const pass = await frameLogin.waitForSelector('input[id="password"]', {visible: true});
     await cpf.type(process.env.CPF);
     await pass.type(process.env.PASS);
     await pass.press('Enter');
+    // await page.click('#btnEntrar');
+
+    // ABRINDO PÁGINA DE PESQUISAR PROCESSO
+    // await new Promise(r => setTimeout(r, 2000));
+    try {
+        await page.waitForSelector('a[title="Abrir menu"]',  { timeout: 1000 });
+    } 
+    catch (error) {
+        await page.reload();
+    }
+    await new Promise(r => setTimeout(r, 1000));
+    await page.click('span.avatar.tip-bottom');
+    await new Promise(r => setTimeout(r, 1000));
+    // const perfil = page.waitForSelector('select[id="papeisUsuarioForm:usuarioLocalizacaoDecoration:usuarioLocalizacao"]', { timeout: 1000 });
+    // console.log(perfil);
+    // await perfil.select("1");
+    const perfil = 'select#papeisUsuarioForm\\:usuarioLocalizacaoDecoration\\:usuarioLocalizacao';
+    const optionValue = '1';
+    await page.select(perfil, optionValue);
+    await new Promise(r => setTimeout(r, 2000));
 
     // OPEN PAGE SEARCH PROCESS
     const openMenu = await page.waitForSelector('a[title="Abrir menu"]');
